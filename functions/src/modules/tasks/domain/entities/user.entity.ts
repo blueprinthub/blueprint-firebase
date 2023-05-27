b9b5734dcdd947f0bae32b0f8aa0a577c7348bd9
Yansaro Rodriguez <yansarorodriguez@Yansaros-Mac-mini.local>
2023-05-27 15:46:38 -0400

    docs(user.entity): improve docs by adding props and example

--- functions/src/modules/tasks/domain/entities/user.entity.ts
+++ functions/src/modules/tasks/domain/entities/user.entity.ts
@@ -2,6 +2,18 @@
  * A representation of a user who is a member of a project. This has an
  * account in the platform that the project is defined in. Don't confuse
  * this user with the user of the app, since these are very different.
+ *
+ * @interface
+ * @property {string} platformUrl - The URL of the user's profile in
+ * the platform.
+ * @property {string} displayName - The display name of the user.
+ * @property {string} avatarUrl - The URL of the user's avatar in the platform.
+ * @example
+ * {
+ *  platformUrl: 'https://jira.com/1234567890',
+ *  displayName: 'John Doe',
+ *  avatarUrl: 'https://jira.com/1234567890/avatar.png'
+ * }
  */
 export interface User {
   /**
